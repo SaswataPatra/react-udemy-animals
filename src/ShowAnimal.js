a3b0b684f16e13223820ed39210881f363de14a1
PoMaMa69 <saswatapatra15@gmail.com>
2023-07-20 14:04:28 +0530

    added heart

--- src/ShowAnimal.js
+++ src/ShowAnimal.js
@@ -5,18 +5,28 @@ import dog from "./svg/dog.svg";
 import gator from "./svg/gator.svg";
 import heart from "./svg/heart.svg";
 import horse from "./svg/horse.svg";
+import { useState } from "react";
 
 const svgMap = {
-    bird,
-    cat,
-    cow,
-    dog,
-    gator,
-    horse
-}
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
 
-function ShowAnimal({type}) {
-    return (<div><img alt="animal" src={svgMap[type]}></img></div>);
+function ShowAnimal({ type }) {
+  const [click, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(click + 1);
+  };
+  return (
+    <div onClick={handleClick}>
+      <img alt="animal" src={svgMap[type]}></img>
+      <img alt="heart" src={heart} style={{width : 10+10*click+'px'}}></img>
+    </div>
+  );
 }
 
 export default ShowAnimal;
